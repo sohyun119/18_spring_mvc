385c4c7caa36ea8ca7fdaf8713c721c6ef411003
DESKTOP-0GQC1V5\19_web_LSH <sh-119@naver.com>
2023-12-15 21:32:51 +0900

    DynamicQuery_Mapper공부

---
+++ src/main/java/com/spring/mvc/chapter03_dynamicQuery/DynamicQueryDAO.java
@@ -0,0 +1,167 @@
+package com.spring.mvc.chapter03_dynamicQuery;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.spring.mvc.dto.BrandDTO;
+import com.spring.mvc.dto.ProductDTO;
+
+@Repository
+public class DynamicQueryDAO {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	
+	// [ if ] 사용예시
+	public void ifEx() {
+		
+		System.out.println("\n --- ifEx --- \n");
+		
+		Map<String,Object> searchMap = new HashMap<String, Object>();
+//		searchMap.put("searchKeyword", "enrollDt");
+//		searchMap.put("searchWord" , "2022");
+		searchMap.put("searchKeyword", "productNm");
+		searchMap.put("searchWord" , "삼성전자");
+		
+		List<ProductDTO> productList = sqlSession.selectList("dynamicQuery.ifEx" , searchMap);
+		for (ProductDTO productDTO : productList) {
+			System.out.println(productDTO);
+		}
+		
+	}
+	
+	
+	// [ when ] 사용예시
+	public void whenEx() {
+		
+		System.out.println("\n --- whenEx --- \n");
+		
+		Map<String,Object> searchMap = new HashMap<String, Object>();
+//		searchMap.put("searchKeyword", "enrollDt");
+//		searchMap.put("searchWord" , "2022");
+		searchMap.put("searchKeyword", "productNm");
+		searchMap.put("searchWord" , "삼성전자");
+		
+		List<ProductDTO> productList = sqlSession.selectList("dynamicQuery.whenEx" , searchMap);
+		for (ProductDTO productDTO : productList) {
+			System.out.println(productDTO);
+		}
+		
+	}
+	
+	
+	// [ otherwise ] 사용예시
+	public void otherwiseEx() {
+		
+		System.out.println("\n --- otherwiseEx --- \n");
+		
+		int deliveryPrice = 0;
+		//int deliveryPrice = 3000;
+		
+		List<ProductDTO> productList = sqlSession.selectList("dynamicQuery.otherwiseEx" , deliveryPrice);
+		for (ProductDTO productDTO : productList) {
+			System.out.println(productDTO);
+		}
+		
+	}
+	
+	
+	// [ foreach ] insert 사용예시 
+	public void foreachEx01() {
+		
+		System.out.println("\n --- foreachEx01 --- \n");
+		
+		List<BrandDTO> brandList = new ArrayList<BrandDTO>();
+		
+		for (int i = 111; i < 120; i++) { // 111 ~ 120 , 121 ~ 130으로 변경하며 테스트 
+			
+			BrandDTO brandDTO = new BrandDTO();
+			brandDTO.setBrandCd("b" + i);
+			brandDTO.setBrandNm("추가된브랜드" + i);
+			brandDTO.setEnteredDt(new Date());
+			brandDTO.setActiveYn("Y");
+			
+			brandList.add(brandDTO);
+			
+		}
+		
+		sqlSession.insert("dynamicQuery.foreachEx01" , brandList);
+		
+	}
+	
+	
+	// [ foreach ] select 사용예시 
+	public void foreachEx02() {
+		
+		System.out.println("\n --- foreachEx02 --- \n");
+		
+		String[] brandCdList = {"b1" , "b2" , "b3"};
+		
+		List<BrandDTO> brandList = sqlSession.selectList("dynamicQuery.foreachEx02" , brandCdList);
+		for (BrandDTO brandDTO : brandList) {
+			System.out.println(brandDTO);
+		}
+		
+	}
+	
+	
+	// [ foreach ] delete 사용예시
+	public void foreachEx03() {
+		
+		System.out.println("\n --- foreachEx03 --- \n");
+		
+		String[] brandCdList = {"b100" , "b101" , "b102" , "b103" , "b104" };
+		
+	}
+	
+	
+	// [ foreach ] update 사용예시
+	public void foreachEx04() {
+		
+		System.out.println("\n --- foreachEx04 --- \n");
+		
+		int[] productCdList = {1,2,3,4,5,6};
+		
+		
+	}
+	
+	
+	// [ where ] 사용예시
+	public void whereEx() {
+		
+		System.out.println("\n --- whereEx --- \n");
+		
+		ProductDTO productDTO = new ProductDTO();
+		// 1) productNm과 brandCd가 모두 있을 경우 > 정상
+		// 2) productNm만 있을 경우 > 정상
+		// 3) brandCd만 있을 경우 > 오류
+		//productDTO.setProductNm("삼성");
+		productDTO.setBrandCd("b1");
+		
+	}
+	
+	
+	// [ set ] 사용예시
+	public void setEx() {
+		
+		System.out.println("\n --- setEx --- \n");
+		
+		ProductDTO productDTO = new ProductDTO();
+		// 1) price와 deliveryPrice가 모두 있을 경우 > 정상
+		// 2) price만 있을 경우 > 오류
+		// 3) deliveryPrice만 있을 경우 > 정상
+		productDTO.setPrice(1);
+		productDTO.setDeliveryPrice(1);
+		
+	}
+	
+	
+}
